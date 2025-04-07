80adce52dc65a8e36e0fb74ffc7289e7df0468d8
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-07 11:24:20 +0800

    created a pseudocode following the requirements

---
+++ quiz_creator.py
@@ -0,0 +1,16 @@
+# Ask user for a quiz name
+
+# Loop while user wants to add questions
+    # Ask user for a question
+    # Ask user for 4 possible answers (a, b, c, d)
+    # Ask user for the correct answer (a/b/c/d))
+
+    # Format question and answers to make it readable
+    # Append the question and answer block to the quiz file
+
+    # Ask user if they want to add another question
+    # If yes, repeat the process
+    # If no, save the quiz and exit
+
+# Print a message indicating the quiz has been saved
+# End program
